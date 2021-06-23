fd2de93bae9f488476deef6d2d382d3ca11f8592
Twikura <rainbowtrash2333@gmail.com>
2021-06-23 21:38:42 +0800

    update notes--std input

--- LearnDotNet/Program.cs
+++ LearnDotNet/Program.cs
@@ -4,13 +4,29 @@ namespace LearnDotNet
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            // Hello World
+            HelloWorld.PrintHelloWorld();
+        }
     }
+    // 学习C#的输入、输出
     class HelloWorld
     {
-        public static void Main()
+        public static void PrintHelloWorld()
         {
             Console.WriteLine("Hello the World!");
+            //Console.ReadKey();
+            Console.Write("Hello My Firends,");
+            //等效为Console.WriteLine(" do you like My little Pony?");
+            Console.Write(" do you like My little Pony?\n");
+            Console.Write("I like it.");
+
+        }
+        public static void HelloMyFriend()
+        {
+            Console.WriteLine("Hi, I'm Twikura, Can you tell me your name?");
+           
         }
     }
 }
